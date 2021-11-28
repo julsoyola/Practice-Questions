64c96e28b957d7f97a2f8dfe99c38d62c0953e0b
Juliana <72620056+julsoyola@users.noreply.github.com>
2021-11-28 13:05:33 -0500

    Create Words.java

---
+++ Words.java
@@ -0,0 +1,51 @@
+import java.io.*; 
+import java.util.*;
+// Add any extra import statements you may need here
+
+
+class Main {
+
+  // Add any helper functions you may need here
+  
+
+  String findEncryptedWord(String s) {
+    // Write your code here
+       // **** initialization ****
+    String r    = "";
+    int mid     = 0;
+ 
+    // **** [0] compute the position of of middle character in S ****
+    mid = s.length() / 2;
+    if (s.length() % 2 == 0)
+        mid -= 1;
+ 
+    // **** [1] append the middle character of S ****
+    r += s.substring(mid, mid + 1);
+ 
+   
+    // **** [2] append the encrypted version of the substring of S 
+    //      that's to the LEFT of the middle character (if non-empty) ****
+    if (mid > 0) {
+ 
+        // **** generate left sub string ****
+        String ls = s.substring(0, mid);
+ 
+        // **** encrypt and append ***
+        r += findEncryptedWord(ls);
+    }
+ 
+    // **** [3] append the encrypted version of the substring of S 
+    //      that's to the RIGHT of the middle character (if non-empty) ****
+    if (mid < s.length() - 1) {
+ 
+        // **** generate right sub string ****
+        String rs = s.substring(mid + 1, s.length());
+ 
+        // **** encrypt and append ****
+        r += findEncryptedWord(rs);;
+    }
+ 
+    // **** return encrypted string ****
+    System.out.print(r + " ");
+    return r;
+  }
